4016e19e11eef05c7caa7144af315af69fd3f1eb
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-12-24 09:29:19 +0100

    setting up mongoose and the User model

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
+const mongoose = require("mongoose");
 
 const app = express();
 dotenv.config();
@@ -8,6 +9,14 @@ dotenv.config();
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DB_URI)
+  .then(() => {
+    console.log("database connect");
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 
 app.listen(port, () => {
   console.log(`server up and running on port: ${port}`);
