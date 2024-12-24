c69f50fc86273165a2251ef96b2eee5c8e183293
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-12-24 13:34:10 +0100

    created a new blog model

---
+++ models/blogModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const User = require("./userModel");
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: User
+    }
+}, {timestamps: true});
+
+const Blog = mongoose.model("Blog", blogSchema);
+module.exports = Blog;
