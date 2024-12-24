a3280c8cc362cd5d13aad24c9273967c28d71a09
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-12-24 11:53:04 +0100

    added a simple homzpage, used express-session to store the username so only logged-in users can access it

--- app.js
+++ app.js
@@ -2,23 +2,31 @@ const express = require("express");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const app = express();
 dotenv.config();
 
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}));
-
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.DB_URI)
+mongoose
+  .connect(process.env.DB_URI)
   .then(() => {
-    console.log("database connect");
+    console.log("database connected");
   })
   .catch((err) => {
     console.log(err);
-  })
+  });
 
 app.listen(port, () => {
   console.log(`server up and running on port: ${port}`);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,67 +3,77 @@ const router = express.Router();
 const User = require("../models/userModel");
 
 router.get("/register", (req, res) => {
-    res.render("register");
+  if (req.session.user) {
+    return res.redirect("/");
+  }
+  res.render("register", { errorMessage: null });
 });
 
 router.post("/register", (req, res) => {
-    const {username, email, password} = req.body;
+  const { username, email, password } = req.body;
 
-    User.findOne({$or: [{username}, {email}]})
-        .then((existingUser) => {
-            if (existingUser) {
-                const errorMessage = "";
-                if (existingUser.email === email) {
-                    errorMessage = "email already exists";
-                }
-                else {
-                    errorMessage = "user already exists";
-                }
-                res.render("register", {errorMessage});
-            }
-            const user = new User({username, email, password});
-            return user.save();
-        })
-        .then(() => {
-            return res.redirect("/");
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
+  User.findOne({ $or: [{ username }, { email }] })
+    .then((existingUser) => {
+      if (existingUser) {
+        let errorMessage = "";
+        if (existingUser.email === email) {
+          errorMessage = "email already exists";
+        } else {
+          errorMessage = "user already exists";
+        }
+        res.render("register", { errorMessage });
+      }
+      const user = new User({ username, email, password });
+      return user.save();
+    })
+    .then((savedUser) => {
+      if (savedUser) {
+        req.session.user = savedUser.username;
+        return res.redirect("/");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 router.get("/login", (req, res) => {
-    res.render("login");
+  if (req.session.user) {
+    return res.redirect("/");
+  }
+  res.render("login", { errorMessage: null });
 });
 
 router.post("/login", (req, res) => {
-    const {username, password} = req.body;
+  const { username, password } = req.body;
 
-    User.findOne({username})
-        .then((foundUser) => {
-            const errorMessage = "";
-            if (foundUser) {
-                if (foundUser.password !== password) {
-                    errorMessage = "invalid information";
-                    return res.render("login", {errorMessage});
-                }
-                else {
-                    console.log(`${foundUser.username} has loggoed in`);
-                    return res.redirect("/");
-                }
-            }
-            else {
-                errorMessage = "username doesn't exist";
-                return res.render("login", {errorMessage});
-            }
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
+  User.findOne({ username })
+    .then((foundUser) => {
+      let errorMessage = "";
+      if (foundUser) {
+        if (foundUser.password !== password) {
+          errorMessage = "invalid information";
+          return res.render("login", { errorMessage });
+        } else {
+          console.log(`${foundUser.username} has loggoed in`);
+          req.session.user = foundUser.username;
+          return res.redirect("/");
+        }
+      } else {
+        errorMessage = "username doesn't exist";
+        return res.render("login", { errorMessage });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 router.get("/", (req, res) => {
-    res.redirect("/login");
-})
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
+  res.render("home", { user: req.session.user });
+});
 
 module.exports = router;
