6997b9de67dbe363d146c5c357251e9872b4522f
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-12-26 10:09:01 +0100

    made the author name of each blog on the hoepage a link that redirects to that user's profile

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Blog = require("../models/blogModel");
+const User = require("../models/userModel");
 
 const router = express.Router();
 
@@ -8,14 +9,51 @@ router.get("/profile", (req, res) => {
     return res.redirect("/login");
   }
   const userId = req.session.userId;
-  Blog.find({author: userId})
-  .sort({createdAt: -1})
-  .then((blogs) => {
-    res.render("profile", {blogs});
-  })
-  .catch((err) => {
-    console.log(err);
-  })
+  Blog.find({ author: userId })
+    .sort({ createdAt: -1 })
+    .then((blogs) => {
+      res.render("profile", { blogs });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+function getUserId(username) {
+  return User.findOne({ username })
+    .then((user) => {
+      if (user) {
+        return user._id;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+router.get("/profile/:user", (req, res) => {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
+  const user = req.params.user;
+  if (user === req.session.user) {
+    return res.redirect("/profile");
+  }
+  getUserId(user)
+    .then((userId) => {
+      if (!userId) {
+        return res.redirect("/");
+      }
+      return Blog.find({ author: userId })
+        .sort({ createdAt: -1 })
+        .populate("author", "username");
+    })
+    .then((blogs) => {
+      res.render("userProfile", { blogs });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
